d2c85ab0a292422725fc6ee3818e58b3b1b31486
Mohamad Elhadidy <elhadidy384@gmail.com>
2025-09-25 16:01:43 +0300

    Brand migration - model - resource

---
+++ app/Filament/Resources/Brands/Tables/BrandsTable.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Filament\Resources\Brands\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class BrandsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('image')->circular(),
+                TextColumn::make('name')->searchable()->sortable(),
+
+                TextColumn::make('description')
+                    ->placeholder('No description')
+                    ->formatStateUsing(fn($state) => str($state)->words(4)),
+
+                ToggleColumn::make('is_active')->label('Active')->sortable(),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+                Filter::make('is_active')
+                    ->query(fn(Builder $query): Builder => $query->where('is_active', true)),
+                SelectFilter::make('is_active')->label('Active')
+                    ->options([
+                        true => 'Active',
+                        false => 'Not Active',
+                    ])
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make()
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+
+    }
+}
